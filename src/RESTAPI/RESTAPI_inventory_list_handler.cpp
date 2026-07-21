850fa99868e057a4951ccc6bf3ceb95e21e7661c
Uttam Vashisth <uttam21.vashisth@routerarchitects.com>
2026-07-21 11:10:51 +0530

    test comment

--- src/RESTAPI/RESTAPI_inventory_list_handler.cpp
+++ src/RESTAPI/RESTAPI_inventory_list_handler.cpp
@@ -10,6 +10,8 @@
 #include "RESTAPI/RESTAPI_db_helpers.h"
 #include "StorageService.h"
 
+// Test comment -----------------------------------------------------------------------------------------------------------------
+
 namespace OpenWifi {
 	void RESTAPI_inventory_list_handler::SendList(const ProvObjects::InventoryTagVec &Tags,
 												  bool SerialOnly) {
